bb9bb06b25a41c02a58d1ef93d65582f57d39c60
iGreenDogs <63960105+iGreenDogs@users.noreply.github.com>
2022-03-15 21:05:32 -0400

    Set up overworld (1.18)

---
+++ src/main/java/net/bobwazheer/mods/standardfood/StandardFoods.java
@@ -0,0 +1,10 @@
+package net.bobwazheer.mods.standardfood;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StandardFoods implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
